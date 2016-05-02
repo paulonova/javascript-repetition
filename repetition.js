ac6e5eb2eb8e65a6161d38234c3c4b6c6dca8f55
Paulo Nova <paulonova@users.noreply.github.com>
2016-05-02 13:15:38 +0200

    Updated repetition.js

--- repetition.js
+++ repetition.js
@@ -1,1 +1,22 @@
-//<JavaScript
+// JavaScript
+
+/*
+  Array
+*/
+
+// En array är...
+// Ett ställe där man lagrar...
+// En samling med saker
+// En lista
+
+// Exempel 1
+// En första array (metod 1)
+// Vi initierar en array (berättar vad som finns i den samtidigt som den skapas)
+var weekdays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"; "Söndag"];
+
+// Hämta ur vår array på följande sätt
+// Skriva ut första veckodagen
+console.log("Exempel 1. Första veckodagen " + weekday[0]);
+
+// Spara i en variabel
+var favoriteDay = weekday[4];
